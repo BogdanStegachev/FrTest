0382c48263604369cacb27bd29ad0bf60816b5b6
Bogdan Stegachev <bogdan.stegachev@mail.ru>
2022-09-23 19:27:44 +0300

    drawing

--- FastReportTest/Form1.Designer.cs
+++ FastReportTest/Form1.Designer.cs
@@ -29,24 +29,44 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(141, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(209, 199);
+            this.panel1.Size = new System.Drawing.Size(200, 209);
             this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(434, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(210, 209);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(141, 270);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(503, 168);
+            this.panel3.TabIndex = 0;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Test";
             this.ResumeLayout(false);
 
         }
@@ -54,6 +74,8 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
--- FastReportTest/Form1.cs
+++ FastReportTest/Form1.cs
@@ -16,8 +16,31 @@ namespace FastReportTest
         public Form1()
         {
             InitializeComponent();
-            
+            ControlMoverOrResizer.Init(panel1);
+            ControlMoverOrResizer.Init(panel2);
+            ControlMoverOrResizer.Init(panel3);
         }
-      
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Figure circle = new Circle(panel1.Height <= panel1.Width ? panel1.Height / 2 : panel1.Width / 2, Color.Black, Color.Black, new Point(0, 0));    
+            e.Graphics.DrawRectangle(new Pen(Color.Black), 0, 0, panel1.Width - 1, panel1.Height - 1);
+            circle.Draw(e.Graphics);
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Figure triangle = new Triangle(1.047, 190, Color.Black, Color.Black, new Point(10, 10));       
+            e.Graphics.DrawRectangle(new Pen(Color.Black), 0, 0, panel2.Width - 1, panel2.Height - 1 );
+            triangle.Draw(e.Graphics);
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            Figure rectangle = new Classes.Rectangle(panel3.Height - 1, panel3.Width - 1, Color.Red, Color.Red, new Point(0, 0));
+            rectangle.Draw(e.Graphics);
+        }
+
     }
 }
